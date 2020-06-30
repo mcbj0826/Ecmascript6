4595dac87821fe93842ff59b40ab0c43a70c56eb
John Fredy <john_fred_y26@hotmail.com>
2020-06-30 17:03:40 -0500

    Clases, Moduos y Generadores.

--- src/es6/index.js
+++ src/es6/index.js
@@ -154,6 +154,51 @@ helloPromise()
     .catch(error => console.log(error))
 
 
+// Manejando clases en ES6
+
+class calculator {
+    constructor() {
+        this.valueA = 0
+        this.valueB = 0
+    }
+
+    sum(valueA, valueB) {
+        this.valueA = valueA
+        this.valueB = valueB
+        return this.valueA + this.valueB
+    }
+}
+
+const calc = new calculator ()
+console.log (calc.sum( 2, 2))
+
+
+// Import y Export en ES6
+
+import { hello} from './module'
+
+hello()
+
+//  Genereitors en ES6
+
+function* helloWorld() {
+    if(true) {
+        yield 'Hello, '
+    }
+    if (true) {
+        yield 'World'
+    }
+}
+
+const generatorHello = helloWorld()
+
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
+
+
+
+
 
 
 
