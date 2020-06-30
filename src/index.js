04cb75655e2c8e75c30566bb7c6f6190dfdc057d
John Fredy <john_fred_y26@hotmail.com>
2020-06-30 18:15:51 -0500

    Async y Await ES8.

--- src/index.js
+++ src/index.js
@@ -23,3 +23,34 @@ console.log(string.padStart(7,'hi'))
 console.log(string.padEnd(12, '-----'))
 console.log('food'.padEnd(12, ' -----'))
 
+// promesas con Async y Await.
+
+const helloWorld =() => {
+    return new Promise ((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve ('Hello World'), 3000)
+            : reject(new Error('test error'))
+
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld()
+    console.log(hello)
+}
+
+helloAsync()
+
+//
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld()
+        console.log(hello)
+    }
+    catch{
+        console.log(error)
+    }
+}
+
+anotherFunction()
