8efadad2284fa93b0341a0662e7c9e1906646734
John Fredy <john_fred_y26@hotmail.com>
2020-06-30 17:19:42 -0500

    Cosas que se implementaron en ES7.

---
+++ src/ES7/index.js
@@ -0,0 +1,17 @@
+// Metodo Includes sirve para mirar si un valor se encuentra dentro de un arreglo.
+
+let numbers = [1,2,3,7,8]
+
+if (numbers.includes(9)) {
+    console.log('Si se encuentra el valor ')
+} else {
+    console.log('Nose encuentra el valor')
+}
+
+// Como elevar a ala potencia con **.
+
+let base = 4
+let exponent = 3
+let result = base ** exponent
+
+console.log(result)
